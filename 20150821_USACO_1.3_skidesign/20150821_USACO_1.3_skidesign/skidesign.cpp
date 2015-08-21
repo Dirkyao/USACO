31e81e2af69ab5b55a7add1e56d06e42bd2b1e53
Dirkyao <981683892@qq.com>
2015-08-21 17:10:58 +0800

    Add the USACO problem 1.3 skidesign

---
+++ 20150821_USACO_1.3_skidesign/20150821_USACO_1.3_skidesign/skidesign.cpp
@@ -0,0 +1,16 @@
+/*
+ID:dirkyao
+PROG:skidesign
+LANG:C++
+*/
+#include<iostream>
+#include<fstream>
+using namespace std;
+
+int main()
+{
+	ifstream fin("skidesign.in");
+	ofstream fout("skidesign.out");
+
+	fin
+}
