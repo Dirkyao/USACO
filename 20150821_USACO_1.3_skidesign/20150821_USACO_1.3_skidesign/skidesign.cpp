97a634e01d5d24203a45c8bf31b19302743f5954
Dirkyao <981683892@qq.com>
2015-08-21 20:33:34 +0800

    Finish the USACO problem 1.3 skidesign

--- 20150821_USACO_1.3_skidesign/20150821_USACO_1.3_skidesign/skidesign.cpp
+++ 20150821_USACO_1.3_skidesign/20150821_USACO_1.3_skidesign/skidesign.cpp
@@ -5,12 +5,57 @@ LANG:C++
 */
 #include<iostream>
 #include<fstream>
+#include<algorithm>
 using namespace std;
+
+int solve(int *hills, int N)
+{
+	int cost = 1000000;
+	if (hills[N - 1] - hills[0] < 18)
+		return 0;
+	else
+	{
+		for (int j = hills[0]; j <= hills[N-1]-17; j++)
+		{
+			int temp = 0;
+			for (int i = 0; i < N; i++)
+			{
+				if (!(hills[i] >= j && hills[i] <= j + 17))
+				{
+					if (hills[i] < j)
+					{
+						temp += (j - hills[i])*(j - hills[i]);
+					}
+					else
+					{
+						temp += (hills[i] - j - 17)*(hills[i] - j - 17);
+					}
+				}
+			}
+			if (temp < cost)
+				cost = temp;
+		}
+		return cost;
+	}
+
+}
 
 int main()
 {
 	ifstream fin("skidesign.in");
 	ofstream fout("skidesign.out");
 
-	fin
+	int N;
+	int hills[1000];
+
+	fin >> N;
+
+	for (int i = 0; i < N; i++)
+	{
+		fin >> hills[i];
+	}
+	sort(hills, hills+N);
+
+	fout << solve(hills, N) << endl;
+	return 0;
 }
