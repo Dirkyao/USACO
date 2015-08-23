01738e3ec08dfb9cc328331bb84977b502939adb
Dirkyao <981683892@qq.com>
2015-08-23 22:40:46 +0800

    Finish the USACO problem 1.4 ariprog

--- 20150823_USACO_1.4_ariprog/20150823_USACO_1.4_ariprog/ariprog.cpp
+++ 20150823_USACO_1.4_ariprog/20150823_USACO_1.4_ariprog/ariprog.cpp
@@ -13,57 +13,65 @@ bool compare(int a, int b)
 {
 	return a < b;
 }
-bool in_arr(int *bisquare,int count, int index, int interval)
+inline bool in_arr(int *bisquare, int count, int index, int interval,int *hash)
 {
 	int goal = bisquare[index] + interval;
-	for (int j = index; j < count; j++)
-	{
-		if (bisquare[j] == goal)
-			return true;
-	}
+	if (hash[goal] == 1)
+		return true;
+
 	return false;
 }
 
-int check_ariprog(int* bisquare,int count,int start_index, int N,int interval)
+inline int check_ariprog(int* bisquare, int count, int start_index, int N, int interval,int* hash)
 {
+	if ((bisquare[start_index] + (N - 1)*interval) > bisquare[count - 1])
+		return 0;
 	int flag = 1;
 	for (int i = 1; i < N; i++)
 	{
-		if (in_arr(bisquare, count, start_index, i*interval))
+		if (in_arr(bisquare, count, start_index, i*interval,hash))
 		{
 			flag++;
 		}
+		else
+			break;
 	}
 	return flag;
 }
 
-void solve(int* bisquare, int count, int N,ofstream * fout)
+inline void solve(int* bisquare, int count, int N,int* hash)
 {
-	for (int i = 1; i < bisquare[count-1]/N; i++)
+	int flag = 0;
+	ofstream fout("ariprog.out");
+	for (int i = 1; i <= bisquare[count - 1] / (N - 1); i++)
 	{
 		for (int j = 0; j <= count - N; j++)
 		{
-			int num = check_ariprog(bisquare, count, j, N, i);
+			int num = check_ariprog(bisquare, count, j, N, i,hash);
 			if (num == N)
 			{
-				*fout << bisquare[j] << i<<endl;
+				fout << bisquare[j] << ' ' << i << endl;
+				flag = 1;
 			}
-			j = j + num-1;
 		}
+	}
+	if (flag == 0)
+	{
+		fout << "NONE" << endl;
 	}
 }
 
 int generate_bisquare(int M, int* bisquare)
 {
-	int *temp = new int((M+1)*(M+1));
+	int *temp = new int[(M + 1)*(M + 1)];
 	for (int i = 0; i <= M; i++)
 	{
 		for (int j = 0; j <= M; j++)
 		{
-			temp[i*(M+1) + j] = i*i + j*j;
+			temp[i*(M + 1) + j] = i*i + j*j;
 		}
 	}
-	sort(temp, temp+(M + 1)*(M + 1), compare);
+	sort(temp, temp + (M + 1)*(M + 1), compare);
 
 	int count = 0;
 	bisquare[count] = temp[count];
@@ -74,21 +82,27 @@ int generate_bisquare(int M, int* bisquare)
 			bisquare[++count] = temp[i];
 		}
 	}
+	cout << "done" << endl;
 	return count + 1;
 }
 
 int main()
 {
 	ifstream fin("ariprog.in");
-	ofstream fout("ariprog.out");
-
 	int N, M;
 	fin >> N >> M;
 
-	int* bisquare = new int((M + 1)*(M + 1));
+	int* bisquare = new int[(M + 1)*(M + 1)];
+	int* hash = new int[M*M * 2];
 
 	int num;
 	num = generate_bisquare(M, bisquare);
 
+	for (int i = 0; i < num; i++)
+	{
+		hash[bisquare[i]] = 1;
+	}
+	solve(bisquare, num, N,hash);
 
+	return 0;
 }
