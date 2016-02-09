ee7a68945d756bb892839310777351afdc44b600
dirkyao <981683892@qq.com>
2016-02-09 23:42:05 +0800

    Finish the USACO problem 2.4 comehome

--- 20160208_USACO_2.4_comehome/20160208_USACO_2.4_comehome/comehome.cpp
+++ 20160208_USACO_2.4_comehome/20160208_USACO_2.4_comehome/comehome.cpp
@@ -7,48 +7,58 @@ LANG:C++
 #include<fstream>
 #include<memory.h>
 #include<queue>
+#include<algorithm>
 using namespace std;
 
 int graph[52][52];
 int cow_position[25];
 int visited[52];
 int my_distance[52];
-struct cmp {
-	bool operator ()(int &a, int &b)
-	{
-		return my_distance[a] > my_distance[b];
-	}
-};
+int inqueue[52];
+
+int cmp(int &a, int &b)
+{
+	return my_distance[a] > my_distance[b];
+}
 
 void dijkstra()
 {
 	memset(visited, 0, sizeof(visited));
+	memset(inqueue, 0, sizeof(inqueue));
 	for (int i = 0;i < 52;i++)
 	{
 		my_distance[i] = 999999;
 	}
 
-	priority_queue<int, vector<int>, cmp> my_queue;
-	my_distance[0] = 0;
-	my_queue.push(52);
-	visited[52] = 1;
+	int my_queue[52];
+	int top = -1;
+	my_distance[25] = 0;
+	my_queue[++top] = 25;
+	visited[25] = 1;
+	inqueue[25] = 0;
 
-	while (!my_queue.empty())
+	while (top!= -1)
 	{
 		for (int i = 0;i < 52;i++)
 		{
-			if (graph[my_queue.top()][i] < 999999 && visited[i] == 0)
+			if (graph[my_queue[top]][i] < 999999 && visited[i] == 0)
 			{
-				my_queue.push(i);
-				visited[i] = 1;
-				int temp_distance = my_distance[my_queue.top()] + graph[my_queue.top()][i];
+				if (inqueue[i] == 0)
+				{
+					my_queue[++top] = i;
+					sort(my_queue, my_queue + top + 1, cmp);
+					inqueue[i] = 1;
+				}
+				int temp_distance = my_distance[my_queue[top]] + graph[my_queue[top]][i];
 				if (temp_distance < my_distance[i])
 				{
 					my_distance[i] = temp_distance;
 				}
 			}
 		}
-		my_queue.pop();
+		visited[my_queue[top]] = 1;
+		top--;
+		sort(my_queue, my_queue + top + 1, cmp);
 	}
 }
 
@@ -77,24 +87,37 @@ int main()
 		char src, dst;
 		int temp_length;
 		fin >> src >> dst >> temp_length;
-		if (src >= 'A'&&src <= 'Y')
+
+		int source, dest;
+		if (src >= 'A'&&src <= 'Z')
 		{
 			cow_position[src - 'A'] = 1;
+			source = src - 'A';
 		}
-		if (dst >= 'A' && dst <= 'Y')
+		else
+		{
+			source = src - 'a' + 26;
+		}
+		if (dst >= 'A' && dst <= 'Z')
 		{
 			cow_position[src - 'A'] = 1;
+			dest = dst - 'A';
 		}
-		if (temp_length < graph[src - 'a'][dst - 'a'])
+		else
 		{
-			graph[src - 'a'][dst - 'a'] = temp_length;
-			graph[dst - 'a'][src - 'a'] = temp_length;
+			dest = dst - 'a' + 26;
+		}
+ 
+		if (temp_length < graph[source][dest])
+		{
+			graph[source][dest] = temp_length;
+			graph[dest][source] = temp_length;
 		}
 	}
 
 	dijkstra();
 	int min = 999999, min_index = -1;
-	for (int i = 26;i < 51;i++)
+	for (int i = 0;i < 25;i++)
 	{
 		if (my_distance[i] < min)
 		{
@@ -102,6 +125,6 @@ int main()
 			min_index = i;
 		}
 	}
-	fout << (char)('a' + min_index) << ' ' << min << endl;
+	fout << (char)('A' + min_index) << ' ' << min << endl;
 	return 0;
 }
