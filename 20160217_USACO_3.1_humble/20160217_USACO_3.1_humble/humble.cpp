dff0dd39bca3b14f1d8c2b6862d9f12e5368690e
dirkyao <981683892@qq.com>
2016-02-19 18:20:17 +0800

    Finish the coding of the USACO problem 3.1 humble

--- 20160217_USACO_3.1_humble/20160217_USACO_3.1_humble/humble.cpp
+++ 20160217_USACO_3.1_humble/20160217_USACO_3.1_humble/humble.cpp
@@ -6,11 +6,14 @@ LANG:C++
 #include<iostream>
 #include<fstream>
 #include<memory.h>
+#include<math.h>
 using namespace std;
 
-int find_min(int* temp,int K)
+long long humble_set[100100];
+
+int find_min(long long *temp,int K)
 {
-	int min = INFINITY;
+	long long min = 9999999999;
 	int min_index = -1;
 	for (int i = 0;i < K;i++)
 	{
@@ -24,22 +27,29 @@ int find_min(int* temp,int K)
 	return min_index;
 }
 
-int simulate(int N,int K, int* humble_set, int* prime_set, int* prime_index)
+int simulate(int N,int K, long long *prime_set, long long *prime_index)
 {
-	int temp[110];
+	long long temp[110];
 	int min_result;
-	prime_index[0] = humble_set[0];
+	humble_set[0] = 1;
 
-	for (int i = 1;i < N;i++)
+	for (int i = 1;i <= N;i++)
 	{
 		for (int j = 0;j < K;j++)
-			temp[j] = humble_set[prime_index[j]] + prime_set[j];
+			temp[j] = humble_set[prime_index[j]] * prime_set[j];
 		min_result = find_min(temp, K);
 
+		for (int j = 0;j < K;j++)
+		{
+			if (temp[j] == temp[min_result])
+			{
+				prime_index[j]++;
+			}
+		}
 		humble_set[i] = temp[min_result];
-		prime_index[min_result]++;
+		//prime_index[min_result]++;
 	}
-	return humble_set[N - 1];
+	return humble_set[N];
 }
 
 int main()
@@ -47,8 +57,8 @@ int main()
 	ifstream fin("humble.in");
 	ofstream fout("humble.out");
 	int K, N;
-	int humble_set[100100];
-	int prime_set[110],prime_index[110];
+	
+	long long prime_set[110],prime_index[110];
 
 	fin >> K >> N;
 	for (int i = 0;i < K;i++)
@@ -57,6 +67,6 @@ int main()
 	}
 	memset(prime_index, 0, sizeof(prime_index));
 
-	fout << simulate(N, K, humble_set, prime_set, prime_index) << endl;
+	fout << simulate(N, K, prime_set, prime_index) << endl;
 	return 0;
 }
