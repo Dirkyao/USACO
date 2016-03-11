5150161dcd97761dcf6439278c0c65c4b6e2ceb2
Dirkyao <dirkyao@sina.com>
2016-03-11 23:11:54 +0800

    Finish the USACO problem 3.1 stamps

--- 20160305_USACO_3.1_stamps/20160305_USACO_3.1_stamps/stamps.cpp
+++ 20160305_USACO_3.1_stamps/20160305_USACO_3.1_stamps/stamps.cpp
@@ -6,56 +6,59 @@ LANG:C++
 #include<iostream>
 #include<fstream>
 #include<memory.h>
+#include<vector>
 using namespace std;
 
 typedef struct node{
-	int stamps[51];
+	int num = 0;
 	int total = 0;
 }postage;
 
+int* total = new int[2100001];
+
 int K, N;
 int stamps[51];
-#define conse_num 50000
+#define conse_num 2100001
 
-int consecutive_dp()
+void consecutive_dp()
 {
 	ofstream fout("stamps.out");
-	postage postages[conse_num];
-	memset(postages[0].stamps, 0, sizeof(postages[0].stamps));
+	int round_num = 0;
+
+	memset(total, 0, sizeof(total));
 
 	for (int i = 1; i < conse_num; i++)
 	{
+
 		int flag = 0;
 		int min_stamps_num = 220;
 		int stamp_choice = -1;
-		memset(postages[i].stamps, 0, sizeof(postages[0].stamps));
+
+		//memset(postages[i].stamps, 0, sizeof(postages[0].stamps));
 
 		for (int j = 0; j < N; j++)
 		{
 			int index = i - stamps[j];
-			if (index >= 0 & postages[index].total + 1 <= min_stamps_num & postages[index].total + 1 < K)
+			if (index >= 0 && total[index] + 1 < min_stamps_num && total[index] + 1 <= K)
 			{
 				flag = 1;
 				stamp_choice = j;
-				min_stamps_num = postages[index].total + 1;
+				min_stamps_num = total[index] + 1;
 			}
 		}
 		
 		if (flag == 1)
 		{
 			int index = i - stamps[stamp_choice];
-			postages[i].total = postages[index].total + 1;
-			for (int j = 0; j < N; j++)
-			{
-				postages[i].stamps[j] = postages[index].stamps[j];
-			}
-			postages[i].stamps[stamp_choice]++;
+			total[i] = total[index] + 1;
 		}
 		else
 		{
-			fout << i - 1 << endl;
+			fout << i-1 << endl;
+			return;
 		}
 	}
+
 }
 
 int main()
